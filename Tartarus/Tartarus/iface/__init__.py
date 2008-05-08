9b9aff7ddc1bbf9d9e112f7f2770d6ad840c62a6
Ivan Melnikov <imelnikov@etersoft.ru>
2008-05-08 20:28:47 +0400

    added ability to load interface modules via statemets like "from Tartarus.iface import ModName"
    also small code and documentation improvements

--- Tartarus/Tartarus/iface/__init__.py
+++ Tartarus/Tartarus/iface/__init__.py
@@ -1,16 +1,27 @@
 
-import sys
+import sys, logging
 from Tartarus import slices
 
 _orig_import = __import__
 
 def tartarus_import(*args):
+    logging.debug("Import hook invoked for name '%s', fromlist = %s" % (args[0],args[3]))
     name = args[0]
     if name.startswith("Tartarus.iface."):
         if not sys.modules.has_key("name"):
             try:
-                mname = name[len("Tartarus.iface."):]
-                slices.load(name)
+                # Strip a prefix from argument.
+                # len("Tartarus.iface.") == 15
+                mname = name[15:]
+                slices.load(mname)
+            except:
+                pass
+
+    elif name == "Tartarus.iface" and len(args) > 3 and args[3] != None:
+        # import in form "from Tartarus.iface import ModuleName1, ModuleName2"
+        for mname in args[3]:
+            try:
+                slices.load(mname)
             except:
                 pass
 
@@ -18,11 +29,22 @@ def tartarus_import(*args):
 
 
 def setup_import_hook():
+    """Setup hook on __import__.
+
+    This function installs hook on builtin __import__ function,
+    which enables autoloading slice files from Tartarus.
+    """
     import __builtin__
     __builtin__.__import__ = tartarus_import
 
 
 class Loader(object):
+    """Attributes of this class are submodules of Tartarus.iface package.
+
+    This modules are imported as needed.
+    """
     def __getattr__(self, name):
-        return __import__("Tartarus.iface." + name)
+        modname = "Tartarus.iface." + name
+        __import__(modname)
+        return sys.modules[name]
 
