747b90bf782ec7e7942595602415d5c0fab3eafb
Ivan Melnikov <imelnikov@etersoft.ru>
2008-05-19 19:47:28 +0400

    implemented reach daemonization facilities

--- Tartarus/Tartarus/daemon.py
+++ Tartarus/Tartarus/daemon.py
@@ -1,161 +1,241 @@
 
-import sys, os, traceback, Tartarus
+from __future__ import with_statement
+import sys, os, traceback, exceptions, Ice, Tartarus
 from Tartarus import logging
 
-def run()
+_msg_len = 10000
 
 
-def _make_jifffile(file, pid):
-    f = open("/proc/%d/stat" % pid)
+def _format_exception():
+    (et, ev, tb) = sys.exc_info()
     try:
-        l = f.readline()
-    finally:
-        f.close()
-    pidfile = open(file, "w")
-    try:
-        pidfile.write(l.split(' ')[21])
-    finally:
-        pidfile.close()
-
-def _check_jifffile(file, pid):
-    f = open("/proc/%d/stat" % pid)
-    try:
-        l = f.readline()
-    finally:
-        f.close()
-    pidfile = open(file)
-    try:
-        j = pidfile.readline()
-    finally:
-        pidfile.close()
-    return l.split(' ')[21] == j
-
-def _make_pid_file(file, pid):
-    f = open(pidfile, "w")
-    f.write("%d\n" % pid)
-    f.close()
-
-def _pid_from_file(file):
-    f = open(file)
-    try:
-        return int(f.readline)
-    finally:
-        f.close()
-
-def run_fork(pidfile, jiffile, args):
-    """Start an application as a daemon."""
-    (rfd,wfd) = os.pipe()
-    pid = os.fork()
-
-    if pid == 0:
-        #child
-        os.setsid()
-        os.close(rfd)
-        return _App(wfd).main(args)
-
-    #parent
-    os.close(wfd)
-    res = os.read(rfd, _msg_len + 3)
-    code = ord(res[0])
-    if (code != 0):
-        logging.error(rfd[1:])
-        return code
-
-    #make pid file
-    if pidfile is not None:
-        _make_pid_file(pidfile, pid)
-
-    if jiffile is not None:
-        _make_jifffile(jiffile, pid)
-
-    return 0
-
-def _usage(name):
-        print "Usage: %s [option] args" % name
-        print """
-Where option is  one of
-    --help              show this message
-    --daemon <pidfile>  start service as a daemon (e.g. detach current console)
-        Daemon's PID will be written to <pidfile>
-    --daemon-ex <pidfile> <jifffile>
-    --status <pidfile>  check wheter the daemon is still running
-    --status-ex <pidfile> <jifffile>
-    --stop <pidfile>   stop the daemon
-    --stop-ex <pidfile> <jifffile>
-        \n"""
-        return -1
-
-def _check_and_get_pid(pidfile, jifffile):
-    pid = _pid_from_file(pidfile)
-    if jifffile is None or _check_jifffile(jiffile):
-        return pid
-    return 0
-
-def status(pidfile, jifffile):
-    if not os.path.isfile(pidfile):
-        print "service is not running"
-        return 1
-    pid = _check_and_get_pid(pidfiole, jifffile)
-    if pid != 0 and os.path.isdir("/proc/%d" % pid):
-        print "service is running"
-        return 0
-    print "service is down, but pidfile is here"
-    return -1
-
-def stop(pidfile, jiffile):
-    if not os.path.isfile(pidfile):
-        print "service is not running"
-        return 1
-    pid = _check_and_get_pid(pidfiole, jifffile)
-    if pid != 0:
-        os.kill(pid, 15)
-
-def _main():
-    l = len(sys.argv)
-    if l > 1:
-        if sys.argv[1] == "--help":
-            return usage(sys.argv[0])
-        elif sys.argv[1] == "--daemon":
-            if l > 2
-                return run_fork(sys.argv[2], None,  sys.argv[3:])
-            else:
-                return usage()
-        elif sys.argv[1] == "--daemon-ex":
-            if l > 3
-                return run_fork(sys.argv[2], sys.argv[3], sys.argv[4:])
-            else:
-                return usage()
-
-        elif sys.argv[1] == "--stop":
-            if l == 3
-                return stop_daemon(sys.argv[2], None)
-            else:
-                return usage()
-        elif sys.argv[1] == "--stop-ex":
-            if l == 4
-                return stop_daemon(sys.argv[2], sys.argv[3])
-            else:
-                return usage()
-
-        elif sys.argv[1] == "--status":
-            if l == 3
-                return status(sys.argv[2], None)
-            else:
-                return usage()
-        elif sys.argv[1] == "--status-ex":
-            if l == 4
-                return status(sys.argv[2], sys.argv[3])
-            else:
-                return usage()
-
-    return run()
-
-def main():
-    try:
-        return _main()
+        return "%s" % ev.reason
     except:
-        traceback.print_exc()
-        return -1
+        pass
+
+    return traceback.format_exception(et,ev,None)
+
+
+class DaemonException(exceptions.Exception):
+    def __init__(self, code, msg):
+        self.code = code
+        self.message = msg
+    def __repr__(self):
+        return self.__class__.__name__ + (": %s" % args)
+
+
+class Daemon(Ice.Application):
+    # clients will override this two:
+    def start(self, comm, args):
+        pass
+    def wait(self):
+        pass
+
+    def __init__(self, fd=None):
+        self.parent_fd = fd
+        Ice.Application.__init__(self)
+
+    def run(self, args):
+        try:
+            self.shutdownOnInterrupt()
+            comm = self.communicator()
+            Ice.setProcessLogger(comm.getLogger())
+
+            self.start(self.communicator(), args)
+
+            if self.parent_fd is not None:
+                os.write(self.parent_fd, '\0%d' % os.getpid())
+        except:
+            msg = _format_exception()
+            if self.parent_fd is not None:
+                os.write(self.parent_fd, chr(1) + msg[:_msg_len])
+            else:
+                logging.error(msg)
+            return 1
+
+        try:
+            return self.wait()
+        except:
+            msg = _format_exception()
+            logging.error(msg)
+            return -1
+
+
+class DaemonController(object):
+    """A class to control the Daemon Of Tartarus.
+
+    The code is written under one very strong assumption: the second field in
+    /proc/<pid>/stat does not contain space characters. This is true for any
+    python program, because it's name is the name of python executable
+    (python).
+    """
+    def __init__(self, what, args, pidfile = None, jfile = None, printpid=False):
+        self.args = args
+        self.pidfile = pidfile
+        self.jfile = jfile
+        self.what = what
+        self.printpid = printpid
+
+    def _make_jfile(self, pid):
+        with open("/proc/%d/stat" % pid) as f:
+            j = f.readline().split()[21]
+        with open(self.jfile, "w") as jfile:
+            jfile.write(j)
+            jfile.write('\n')
+
+    def _check_jfile(self, pid):
+        with open("/proc/%d/stat" % pid) as f:
+            l = f.readline()
+        with open(self.jfile) as jfile:
+            j = jfile.readline()
+
+        return l.split()[21] == j
+
+    def _make_pid_file(self, pid):
+        with open(self.pidfile, "w") as f:
+            f.write("%d\n" % pid)
+
+    def _check_and_get_pid(self):
+        if not os.path.isfile(self.pidfile):
+            raise DaemonException, (-1, "service is not running")
+        with open(self.pidfile) as f:
+            pid = int(f.readline())
+        if not os.path.isdir("/proc/%d" % pid):
+            raise DaemonException, (1, "service is not running, but pidfile exists")
+
+        if self.jfile is not None and not self._check_jfile(pid):
+            raise DaemonException, (1, "service is not running, but pidfile exists")
+
+        return pid
+
+    def status(self):
+        return self._check_and_get_pid()
+
+    def stop(self):
+        try:
+            pid = self._check_and_get_pid()
+            os.kill(pid, 15)
+        finally:
+            if self.pidfile and os.path.isfile(self.pidfile):
+                os.unlink(self.pidfile)
+            if self.jfile and os.path.isfile(self.jfile):
+                os.unlink(self.jfile)
+        return 0
+
+    def _run_child(self, parent_fd):
+        try:
+            import signal
+            signal.signal(signal.SIGHUP, signal.SIG_IGN)
+
+            os.setsid()
+            os.chdir('/')
+
+            devnull = os.open("/dev/null", os.O_RDWR, 0)
+            if (devnull > 0):
+                os.dup2(devnull,0)
+                os.dup2(devnull,1)
+                os.dup2(devnull,2)
+
+            if os.fork() > 0:
+                sys.exit(0)
+
+            sys.exit(self.what(parent_fd).main(self.args))
+        except OSError, err:
+            os.write(parent_fd, chr(err.errno) + err.strerror)
+            sys.exit(-1)
+        except:
+            if sys.exc_info()[0] is SystemExit:
+                raise
+            msg = _format_exception()
+            os.write(parent_fd, chr(1) + msg[:_msg_len])
+            sys.exit(-1)
+
+
+
+
+    def start(self):
+        """Start an application as a daemon."""
+
+        (rfd,wfd) = os.pipe()
+
+        if os.fork() == 0:
+            os.close(rfd)
+            self._run_child(wfd)
+            sys.exit(0)
+
+        #parent
+        os.close(wfd)
+        res = os.read(rfd, _msg_len + 3)
+        if len(res) < 1:
+            raise DaemonException, (-1, "Failed to get daemon initialization status")
+        code = ord(res[0])
+        msg = res[1:]
+        if (code != 0):
+            raise DaemonException, (code, msg)
+
+        pid = int(msg)
+        if (self.printpid):
+            print str(pid)
+
+        if self.pidfile is not None:
+            self._make_pid_file(pid)
+        if self.jfile is not None:
+            self._make_jfile(pid)
+
+        return 0
+
+def _parse_options():
+    import optparse
+    usage = "usage: %prog [options] [start|stop|status] [-- [ice_opts]]"
+    version = "%prog 0.0.1"
+
+    parser = optparse.OptionParser(usage=usage, version=version)
+
+    parser.add_option("-p", "--pidfile",
+            help="store id of started process to file PIDFILE")
+    parser.add_option("-j", "--jfile",
+            help="create a jfile JFILE")
+    parser.add_option("-o", "--printpid",
+            action="store_true",
+            help="Optput pid of started process to strout")
+
+    try:
+        sp = sys.argv.index('--')
+        our_args = sys.argv[1:sp]
+        args = sys.argv[(sp+1):]
+    except ValueError:
+        our_args = sys.argv[1:]
+        args = []
+
+    (opts, action) = parser.parse_args(our_args)
+    if len(action) != 1 or action[0] not in ['start','stop','status','run']:
+        parser.error("Don't know what to do!")
+
+    return (opts, action[0], [sys.argv[0]] + args)
+
+
+
+def main(what):
+    try:
+        (opts, action, args) = _parse_options()
+        if action == 'run':
+            return what().main(args)
+        dc = DaemonController(what, args, opts.pidfile, opts.jfile, opts.printpid)
+        if action == 'start':
+            return dc.start()
+        elif action == 'stop':
+            return dc.stop()
+        elif action == 'status':
+            if dc.status():
+                print "service is running"
+                return 0
+
+        raise DaemonError, (-1, "This can't happen!")
+    except DaemonException, ex:
+        sys.stderr.write("Error: ")
+        sys.stderr.write(ex.message)
+        sys.stderr.write('\n')
+        return ex.code
 
 
 
--- Tartarus/Tartarus/run1.py
+++ Tartarus/Tartarus/run1.py
@@ -1,31 +1,10 @@
 
-import os, sys, traceback, Ice, Tartarus
+import Tartarus
 
-from Tartarus import logging
-
-_msg_len = 10000
-
-#
-# TODO: Put this comment to a place where documentation belongs.
-#
-# FORKING.
-#
-# We use the following protocol: before forking, a process creates pipe via
-# os.pipe function. After forking, child does the initialization, 
-# and writes to the pipe a single byte, a zero, if it successfully started.
-# On error, it writes to the pipe a single non-zero byte (a error code?)
-# and the error message, but the message shold not be longer then _msg_len.
-# Parent proccess reads from the pipe, and so blocks until child finishes
-# initialization or fails.
-#
+from Tartarus import daemon, modules
 
 
-
-
-class _App(Ice.Application):
-    def __init__(self, fd = -1):
-        self.parent_fd = fd
-
+class _App(daemon.Daemon):
     def apply_properties(self, props):
         Tartarus.modules.trace = props.getPropertyAsInt("Tartarus.modules.Trace")
         Tartarus.slices.trace = props.getPropertyAsInt("Tartarus.import.Trace")
@@ -35,190 +14,19 @@ class _App(Ice.Application):
             props.getPropertiesForPrefix('Tartarus.addSlicePath.').itervalues()
 
 
-    def run(self, args):
-        try:
-            self.shutdownOnInterrupt()
-            comm = self.communicator()
-            Ice.setProcessLogger(comm.getLogger())
-            adapter = comm.createObjectAdapter("TartarusAdapter")
-
-            self.apply_properties(comm.getProperties())
-
-            Tartarus.modules.load_modules1(adapter)
-
-            adapter.activate()
-            if self.parent_fd > 0:
-                os.write(self.parent_fd, '\0')
-
-            adapter.waitForDeactivate()
-            return 0
-
-        except:
-            msg = traceback.format_exc()
-            if self.parent_fd > 0:
-                os.write(self.parent_fd, chr(1))
-                os.write(self.parent_fd, msg[:_msg_len])
-            else:
-                logging.error(msg)
-            return 1
+    def start(self, comm, args):
+        self.adapter = comm.createObjectAdapter("TartarusAdapter")
+        self.apply_properties(comm.getProperties())
+        modules.load_modules1(self.adapter)
+        self.adapter.activate()
 
 
-
-def run(args):
-    """Just run the application."""
-    return _App().main(args)
-
-
-def _make_jifffile(file, pid):
-    f = open("/proc/%d/stat" % pid)
-    try:
-        l = f.readline()
-    finally:
-        f.close()
-    pidfile = open(file, "w")
-    try:
-        pidfile.write(l.split(' ')[21])
-    finally:
-        pidfile.close()
-
-def _check_jifffile(file, pid):
-    f = open("/proc/%d/stat" % pid)
-    try:
-        l = f.readline()
-    finally:
-        f.close()
-    pidfile = open(file)
-    try:
-        j = pidfile.readline()
-    finally:
-        pidfile.close()
-    return l.split(' ')[21] == j
-
-def _make_pid_file(file, pid):
-    f = open(pidfile, "w")
-    f.write("%d\n" % pid)
-    f.close()
-
-def _pid_from_file(file):
-    f = open(file)
-    try:
-        return int(f.readline)
-    finally:
-        f.close()
-
-def run_fork(pidfile, jiffile, args):
-    """Start an application as a daemon."""
-    (rfd,wfd) = os.pipe()
-    pid = os.fork()
-
-    if pid == 0:
-        #child
-        os.setsid()
-        os.close(rfd)
-        return _App(wfd).main(args)
-
-    #parent
-    os.close(wfd)
-    res = os.read(rfd, _msg_len + 3)
-    code = ord(res[0])
-    if (code != 0):
-        logging.error(rfd[1:])
-        return code
-
-    #make pid file
-    if pidfile is not None:
-        _make_pid_file(pidfile, pid)
-
-    if jiffile is not None:
-        _make_jifffile(jiffile, pid)
-
-    return 0
-
-def _usage(name):
-        print "Usage: %s [option] args" % name
-        print """
-Where option is  one of
-    --help              show this message
-    --daemon <pidfile>  start service as a daemon (e.g. detach current console)
-        Daemon's PID will be written to <pidfile>
-    --daemon-ex <pidfile> <jifffile>
-    --status <pidfile>  check wheter the daemon is still running
-    --status-ex <pidfile> <jifffile>
-    --stop <pidfile>   stop the daemon
-    --stop-ex <pidfile> <jifffile>
-        \n"""
-        return -1
-
-def _check_and_get_pid(pidfile, jifffile):
-    pid = _pid_from_file(pidfile)
-    if jifffile is None or _check_jifffile(jiffile):
-        return pid
-    return 0
-
-def status(pidfile, jifffile):
-    if not os.path.isfile(pidfile):
-        print "service is not running"
-        return 1
-    pid = _check_and_get_pid(pidfiole, jifffile)
-    if pid != 0 and os.path.isdir("/proc/%d" % pid):
-        print "service is running"
+    def wait(self):
+        self.adapter.waitForDeactivate()
         return 0
-    print "service is down, but pidfile is here"
-    return -1
 
-def stop(pidfile, jiffile):
-    if not os.path.isfile(pidfile):
-        print "service is not running"
-        return 1
-    pid = _check_and_get_pid(pidfiole, jifffile)
-    if pid != 0:
-        os.kill(pid, 15)
-
-def _main():
-    l = len(sys.argv)
-    if l > 1:
-        if sys.argv[1] == "--help":
-            return usage(sys.argv[0])
-        elif sys.argv[1] == "--daemon":
-            if l > 2
-                return run_fork(sys.argv[2], None,  sys.argv[3:])
-            else:
-                return usage()
-        elif sys.argv[1] == "--daemon-ex":
-            if l > 3
-                return run_fork(sys.argv[2], sys.argv[3], sys.argv[4:])
-            else:
-                return usage()
-
-        elif sys.argv[1] == "--stop":
-            if l == 3
-                return stop_daemon(sys.argv[2], None)
-            else:
-                return usage()
-        elif sys.argv[1] == "--stop-ex":
-            if l == 4
-                return stop_daemon(sys.argv[2], sys.argv[3])
-            else:
-                return usage()
-
-        elif sys.argv[1] == "--status":
-            if l == 3
-                return status(sys.argv[2], None)
-            else:
-                return usage()
-        elif sys.argv[1] == "--status-ex":
-            if l == 4
-                return status(sys.argv[2], sys.argv[3])
-            else:
-                return usage()
-
-    return run()
 
 def main():
-    try:
-        return _main()
-    except:
-        traceback.print_exc()
-        return -1
-
+    import sys
+    sys.exit(daemon.main(_App))
 
