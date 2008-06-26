43ca9bcdec5ecc2a6e14907357101258b29257b8
Ivan Melnikov <imelnikov@etersoft.ru>
2008-06-26 12:30:32 +0400

    improved module and configuration loading
    - introduced Tartarus.configDir property: every file in
      thus directory is considered to be configuration file and loaded
    - now daemon loads only modules refered to in properties
      which start with 'Tartarus.module.' prefix, and does not
      probe filesystem

--- Tartarus/Tartarus/modules.py
+++ Tartarus/Tartarus/modules.py
@@ -7,17 +7,31 @@ path = []
 trace = 0
 
 def load_module(modname, adapter):
-    # we load only modules which names start with big latin letters
-    if modname[0] < 'A' or modname[0] > 'Z':
-        if trace > 5:
-            logging.trace(__name__, "Skipping module %s" % modname)
-        return
 
     logging.trace(__name__, "Loading module %s." % modname, trace)
-    modname = "Tartarus.%s" % modname
-    __import__(modname)
-    module = sys.modules[modname]
+
+    try:
+        modname = "Tartarus.%s" % modname
+        __import__(modname)
+        module = sys.modules[modname]
+    except (ImportError, KeyError):
+        logging.error("Failed to load module %s, skipping." % modname)
+        return
     module.init(adapter)
+
+def _load_config(props, path):
+    if not os.path.isdir(path):
+        logging.error("Invalid path to configuration files: %s" % path)
+        return
+
+    for f in os.listdir(path):
+        if not os.path.isfile(f):
+            continue
+        try:
+            props.load(f)
+        except:
+            logging.error("Failed to load configuration file: %s" % f)
+
 
 def load_modules1(adapter):
     #if no value specified yet, append the default
@@ -27,31 +41,25 @@ def load_modules1(adapter):
 
     #update module search path
     for p in path:
+        if not os.path.isdir(p):
+            logging.trace(__name__, "Module path not found: %s" % p, trace)
+            continue
         if p not in Tartarus.__path__:
             Tartarus.__path__.append(p)
 
     props = adapter.getCommunicator().getProperties()
+
+    conf_dir = props.getPropertyWithDefault(
+            "Tartarus.configDir", "/etc/Tartarus")
+    _load_config(props, conf_dir)
+
     d = props.getPropertiesForPrefix("Tartarus.module.") #note '.' at the end
-    if d == {}:
-        #load everything we can find
-        logging.trace(__name__, "Loading all modules from path.", trace > 16)
-        mods = _walk_path(path)
-    else:
-        mods = d.itervalues()
 
-    for m in mods:
-        load_module(m, adapter)
-
-
-def _walk_path(p):
-    for dir in p:
-        if not os.path.isdir(dir):
-            logging.warning("Directory from Tartarus.modpath not found: %s"
-                            % dir)
-            continue
-        for m in os.listdir(dir):
-            if os.path.isdir(os.path.join(dir,m)):
-                yield m
-            elif trace > 5:
-                logging.trace(__name__, "Skipping %s from %s" % (m, dir))
+    for m in d.itervalues():
+        # we load only modules which names start with big latin letters
+        if modname[0] < 'A' or modname[0] > 'Z':
+            if trace > 5:
+                logging.trace(__name__, "Skipping module %s" % modname)
+        else:
+            load_module(m, adapter)
 
