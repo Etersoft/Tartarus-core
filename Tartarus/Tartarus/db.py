0de69746d6f5b6620e123ec1edd01e34d6ed12b8
Ivan Melnikov <imelnikov@etersoft.ru>
2008-07-05 21:25:26 +0400

    updated db submodule
    - support of sqlite3 added
    - couple of bugs fixed

--- Tartarus/Tartarus/db.py
+++ Tartarus/Tartarus/db.py
@@ -14,6 +14,7 @@ def _engine2module(engine):
     _engines_mapping = {
             'sqlite'        : 'sqlite',
             'sqlite2'       : 'sqlite',
+            'sqlite3'       : 'sqlite3',
             'postgresql'    : 'psycopg2',
             'postgres'      : 'psycopg2'
             }
@@ -28,7 +29,30 @@ def _strip_prefix(prefix, d):
     return dict(gen)
 
 def _query2string(q,p):
-    return q % tuple(("'%s'" % x for x in params))
+    return q % tuple(("'%s'" % x for x in p))
+
+class _dict_translator(object):
+    def params(self, params):
+        l = len(params)
+        gen = ( ("p%d" % i, params[i]) for i in xrange(0,l) )
+        return dict(gen)
+
+    def query(self, q, l):
+        psubst = tuple( ("%%(p%d)s" % i for i in xrange(0,l)) )
+        w = q % psubst
+        return w
+
+class _qmark_translator(object):
+    def params(self, params):
+        return params
+    def query(self, q, l):
+        return q % tuple( ('?' for x in xrange(0,l)) )
+
+_translator_map = {
+        'pyformat'      : _dict_translator,
+        'qmark'         : _qmark_translator
+        }
+
 
 class _Helper(object):
     engine = None       # database angine (a string) as defined in options
@@ -36,10 +60,7 @@ class _Helper(object):
     module = None       # module corresponding to database engine
     modname = None      # name of database module
     trace = None        # if if > 16 trace queries (quite slow)
-
-    def __add_option(self, d, key):
-        if self.prefix + key in d:
-            self.options[key] = d[key]
+    trans = None
 
     def __init__(self, opts, iface):
         self.ConfigError = iface.ConfigError
@@ -52,16 +73,18 @@ class _Helper(object):
             self.engine = opts['engine']
         except KeyError:
             raise self.ConfigError("Database engine not specified",
-                                prefix + 'engine')
+                                opts['prefix'] + 'engine')
         try:
             self.modname = _engine2module(self.engine)
             __import__(self.modname)
             self.module = sys.modules[self.modname]
+            self.trans = _translator_map[self.module.paramstyle]()
         except (KeyError, ImportError):
             raise self.ConfigError(
                     "Database engine not supported", self.engine)
 
         self.Error = self.module.Error
+
 
         for k in ['dsn', 'user', 'password', 'port', 'host', 'database']:
             if k in opts:
@@ -77,37 +100,26 @@ class _Helper(object):
     def get_connection(self):
         return self.module.connect(**self.options)
 
-
-    def __make_dict(self, params):
-        l = len(params)
-        gen = ( ("p%d" % i, params[i]) for i in xrange(0,l) )
-        return dict(gen)
-
-    def __translate_query(self, q, l):
-        psubst = tuple( ("%%(p%d)s" % i for i in xrange(0,l)) )
-        w = q % psubst
-        return w
-
     def execute(self, con, query, *params):
         cur = con.cursor()
-        q = self.__translate_query(query, len(params))
-        p = self.__make_dict(params)
+        q = self.trans.query(query, len(params))
+        p = self.trans.params(params)
         if self.trace > 16:
-            logging.trace(__name__, "Query on database %s:\n%s",
-                    self.opts['database'],
-                    _query2string(query, params))
+            logging.trace(__name__,
+                    "Query on database %s:\n%s" %
+                    (self.options['database'], _query2string(query, params)))
         cur.execute(q, p)
         return cur
 
     def execute_many(self, con, query, mparams):
         cur = con.cursor()
         l = query.count('%s')
-        q = self.__translate_query(query, l)
-        p = [self.__make_dict(p) for p in mparams]
+        q = self.trans.query(query, l)
+        p = [self.trans.params(p) for p in mparams]
         if self.trace > 16:
             logging.trace(__name__,
                     "Multiple query on database %s, first is:\n%s",
-                    self.opts['database'], _query2string(query, params))
+                    self.options['database'], _query2string(query, params))
         cur.executemany(q, p)
         return cur
 
@@ -127,8 +139,8 @@ class _Helper(object):
 def wrap(msg):
     def decor(method):
         def wrapper(self, *pargs, **kwargs):
+            h = self._dbh
             try:
-                h = self._dbh
                 return method(self,
                         h.get_connection(),
                         *pargs, **kwargs)
@@ -142,5 +154,7 @@ def wrap(msg):
     return decor
 
 def make_helper(opts, prefix, iface):
-    return _Helper(_strip_prefix(prefix, opts), iface)
+    d = _strip_prefix(prefix, opts)
+    d['prefix'] = prefix
+    return _Helper(d, iface)
 
