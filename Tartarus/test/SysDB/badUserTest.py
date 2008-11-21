e5ba6fcb2844b95e4e7bbba0ffe22b5c5225b5db
Ivan Melnikov <imelnikov@etersoft.ru>
2008-11-21 18:38:37 +0300

    SysDB: updated tests
    - fixed tests to work after previous commit
    - two new tests written to check user and group name validator work

---
+++ Tartarus/test/SysDB/badUserTest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+from testbase import *
+
+class badUserTest(TestWithUser):
+    def testCreate(self):
+        ur = self.I.UserRecord(-1, self.gid, '1' + self.name,
+                               self.name, '/bin/bash')
+        self.assertRaises(self.ICore.ValueError, self.um.create, ur)
+
+    def testCreate(self):
+        ur = self.I.UserRecord(-1, self.gid, self.name,
+                               self.name, '/bin/bash')
+        ur.uid = self.um.create(ur)
+
+        ur2 = self.um.getByName(self.name)
+        self.assertEqual(ur, ur2)
+
+        ur.name = '1' + self.name + ', modified'
+        self.assertRaises(self.ICore.ValueError, self.um.modify, ur)
+
+tests = load(badUserTest)
+
+if __name__ == '__main__':
+    main(tests)
+
