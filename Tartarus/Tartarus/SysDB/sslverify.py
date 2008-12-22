1a07910dfad90785fdef23a1d9f1fee0004fe4f7
Ivan Melnikov <imelnikov@etersoft.ru>
2008-12-22 18:30:54 +0300

    Code cleanup and minor bugfixes

--- Tartarus/Tartarus/SysDB/sslverify.py
+++ Tartarus/Tartarus/SysDB/sslverify.py
@@ -1,15 +1,7 @@
 
 import IceSSL
-from Tartarus import db, logging, auth
+from Tartarus import logging, auth
 
-
-_query = """
-SELECT count(group_entries.id)
-FROM group_entries, users
-WHERE users.id == group_entries.userid
-AND users.name == %s
-AND group_entries.groupid IN
-"""
 
 class SimpleGroupVerifier(object):
     def __init__(self, verifier, groups):
@@ -18,10 +10,6 @@ class SimpleGroupVerifier(object):
     def __call__(self, info):
         try:
             return self._verifier.do_verify(info)
-        except self._dbh.DBError, e:
-            logging.warning("Database exception: %s: %s"
-                            % (e.message, e.reason))
-            return False
         except Exception:
             return False
 
