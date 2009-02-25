1a097b1089148815704a5093ea437df611f5fa45
Dmitry M. Maslennikov <rlz@etersoft.ru>
2009-02-25 15:05:41 +0300

    Improve client initialization
    - New Tartarus.client module with initialize function
    - DHCP: t-dhcp now use Tartarus.client module
    
    - Remove unneeded client adn common subpackages

---
+++ Tartarus/Tartarus/client.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import Ice
+import Tartarus
+
+def initialize(cfgs='common', prefixes=[]):
+    cfgs = _to_list(cfgs)
+    props = Ice.createProperties()
+    argv = sys.argv[:]
+    _parse_cmd_line(props, prefixes, argv)
+    cfg = props.getPropertyWithDefault('Tartarus.Config', None)
+    if cfg:
+        props.load(cfg)
+        return _init(props), argv
+    if 'TARTARUS_CONFIG' in os.environ:
+        props.load(os.environ['TARTARUS_CONFIG'])
+        return _init(props), argv
+    for cfg in cfgs:
+        props.load(cfg)
+    return _init(props), argv
+
+def _parse_cmd_line(props, prefixes, argv):
+    props.parseIceCommandLineOptions(argv)
+    prefixes = _to_list(prefixes) + ['Tartarus']
+    for i in prefixes:
+        props.parseCommandLineOptions(i, argv)
+
+def _init(props):
+    def _prefixed(name):
+        return props.getPropertiesForPrefix(name).itervalues()
+    Tartarus.slices.trace = props.getPropertyAsInt("Tartarus.import.Trace")
+    Tartarus.slices.path += _prefixed('Tartarus.addSlicePath.')
+    idata = Ice.InitializationData()
+    idata.properties = props
+    return Ice.initialize(idata)
+
+def _to_list(vals):
+    if isinstance(vals, str):
+        return vals.split()
+    return vals
+
