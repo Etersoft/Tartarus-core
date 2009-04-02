c5b4ab83ee511c12c957807a81445602d7216663
Dmitry M. Maslennikov <rlz@etersoft.ru>
2009-04-02 17:11:16 +0400

    Remove old deploy and join code
    New utility t-dhcpreg also has been added

--- Tartarus/Tartarus/deploy/__init__.py
+++ Tartarus/Tartarus/deploy/__init__.py
@@ -1,24 +1,3 @@
-
-
-__all__ = ['deploy_sysdb', 'deploy_kadmin', 'deploy_dns',
-           'save_keys', 'client']
-
-def deploy_sysdb(comm, opts):
-    import Tartarus
-    import Tartarus.deploy.SysDB
-    Tartarus.deploy.SysDB.deploy_sysdb(comm, opts)
-
-
-def deploy_kadmin(comm, opts):
-    import Tartarus
-    import Tartarus.deploy.Kadmin5
-    Tartarus.deploy.Kadmin5.deploy_kadmin(comm, opts)
-
-def deploy_dns(comm, opts):
-    import Tartarus
-    import Tartarus.deploy.DNS
-    Tartarus.deploy.DNS.deploy_dns(comm, opts)
-
 def save_keys(spr, keytab=None):
     import kadmin5, os
 
