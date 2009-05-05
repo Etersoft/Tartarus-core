9d68afb0130981fc0b0f6f9d6d26cf43ad3f114a
Helen V. Gurevich <lbeasty@altlinux.org>
2009-05-05 19:23:05 +0400

    Feature #146. Begin.

--- Tartarus/Tartarus/system/check.py
+++ Tartarus/Tartarus/system/check.py
@@ -1,27 +1,68 @@
-from hostname import getdomain
+import Tartarus.system.hostname as hostname
 from dns.resolver import query, NXDOMAIN
 from Tartarus.system import Error
 
-def check_krb5_lookup(domain = None):
+def check_krb5_realm(domain = None):
     if not domain:
-        domain = getdomain()
-
+        try:
+            domain = hostname.getdomain()
+        except:
+            raise Error('Can\'t get domain name.\n Please check your network instalation.\n Aborted.')
+    realm_query = '_kerberos.' + domain
     try:
-        realm_query = '_kerberos.' + domain
         realm = iter(query(realm_query,'TXT')).next().strings[0]
+        return realm
     except NXDOMAIN:
         raise Error('kerberos realm for domain "%s" not found'
                     % domain)
 
+def check_krb5_kdc(realm):
     try:
         kdc_query = '_kerberos._udp.' + realm + '.'
-        iter(query(kdc_query,'SRV')).next()
+        return iter(query(kdc_query,'SRV')).next()
     except NXDOMAIN:
         raise Error('kdc for REALM "%s" not found' % realm)
 
+def check_krb5_passwd(realm):
     try:
         kdc_query = '_kpasswd._udp.' + realm + '.'
-        iter(query(kdc_query,'SRV')).next()
+        return iter(query(kdc_query,'SRV')).next()
     except NXDOMAIN:
         raise Error('kpasswd for REALM "%s" not found' % realm)
 
+def check_krb5_lookup():
+    realm = check_krb5_realm()
+    check_krb5_kdc(realm)
+    check_krb5_passwd(realm)
+
+def check_host(host = None):
+    if not host:
+        try:
+            host = hostname.getname()
+        except:
+            raise Error('Can\'t get hostname.\n Please check your network instalation.\n Aborted.')
+
+def check_domain(domain = None):
+    if not domain:
+        try:
+            domain = hostname.getdomain()
+        except:
+            raise Error('Can\'t get domain name.\n Please check your network instalation.\n Aborted.')
+
+    if domain.find('.') < 0:
+        raise Error('Can\'t enter in to \'%s\' domain.\n Please check your domain name: it must have points.\n Aborted.' % domain)
+    if len(domain) < 2:
+        raise Error('Can\'t enter in to \'%s\' domain.\n Please check your domain name: it is too small.\n Aborted.' % domain)
+    if domain.endswith('.localdomain') or domain.endswith('.localdomain.'):
+        raise Error('Can\'t enter in to \'%s\' domain.\n Please check your domain name: it must not be \'localdomain\'.\n Aborted.' % domain)
+
+def check_fqdn(fqdn = None):
+    if not fqdn:
+        try:
+            fqdn = hostname.getfqdn()
+        except:
+            raise Error('Can\'t get full hostname.\n Please check your network instalation.\n Aborted.')
+    if fqdn.find('.') < 0:
+        raise Error('Uncorrect full hostname \'%s\'.\n Please check your full hostname: it must have points.\n Aborted.' % fqdn)
+    if len(fqdn) < 3:
+        raise Error('Uncorrect full hostname \'%s\'.\n Please check your full hostname: it is too small.\n Aborted.' % fqdn)
