38ec38f5be2380f7728755cee42eec910767340c
Helen V. Gurevich <lbeasty@altlinux.org>
2009-05-14 22:18:54 +0400

    t-check was renamed t-diag. to t-diag was added creation of temporary krb5.conf

--- Tartarus/Tartarus/system/check.py
+++ Tartarus/Tartarus/system/check.py
@@ -23,7 +23,7 @@ def _serverTimePrx(domain):
 def _getLocalTime():
     return int(time.time())
 
-def check_Time(domain = None):
+def _compare_Time(domain = None):
     if not domain:
         try:
             domain = hostname.getdomain()
@@ -36,8 +36,20 @@ def check_Time(domain = None):
         localTm = _getLocalTime()
         if abs(serverTm-localTm) > 180:
             raise Error(' UTC time on server is not equal to utc local time.\n Please set time.\n Aborted.')
+        return "Difference in time: %i" % (abs(serverTm-localTm))
     except Ice.ObjectNotExistException:
         raise ObjectNotExists(' Object Time doesn\'t exist')
+
+def check_Time(domain = None):
+    success = []
+    error = []
+    try:
+        time = _compare_Time()
+    except Error, e:
+        error.append('%s' % e)
+    else:
+        success.append('%s sec' % time)
+    return success, error
 
 def _serverDNSPrx(domain):
     pr = Ice.createProperties()
@@ -48,18 +60,30 @@ def _serverDNSPrx(domain):
     prx = comm.stringToProxy(' DNS/Server: ssl -h %s -p 12345' % domain)
     return IcePy.ObjectPrx.checkedCast(prx)
 
-def check_DNS(domain = None):
+def _exist_DNS(domain = None):
     if not domain:
         try:
             domain = hostname.getdomain()
         except:
-            raise Error(' Can\'t get domain name.\n Please check your network instalation.\n Aborted.')
+            raise Error('DNS: Can\'t get domain name.\n Please check your network instalation.\n Aborted.')
     try:
         ob = _serverDNSPrx(domain)
         if ob == None:
-            raise Error(' DNS is not responding')
+            raise Error('DNS: DNS is not responding')
+        return "DNS: DNS exists and is responding"
     except Ice.ObjectNotExistException:
-        raise ObjectNotExists(' Object doesn\'t exist')
+        raise ObjectNotExists('DNS: Object doesn\'t exist')
+
+def check_DNS(domain = None):
+    success = []
+    error = []
+    try:
+        dns = _exist_DNS()
+    except Error, e:
+        error.append('%s' % e)
+    else:
+        success.append('%s' % dns)
+    return success, error
 
 def _check_krb5_realm(domain = None):
     if not domain:
@@ -70,7 +94,7 @@ def _check_krb5_realm(domain = None):
     realm_query = '_kerberos.' + domain
     try:
         realm = iter(query(realm_query,'TXT')).next().strings[0]
-        return realm
+        return realm, 'Kerberos realm for domain "%s" is %s' % (str(domain),str(realm))
     except NXDOMAIN:
         raise Error(' Kerberos realm for domain "%s" not found'
                     % domain)
@@ -78,85 +102,159 @@ def _check_krb5_realm(domain = None):
 def _check_krb5_kdc(realm):
     try:
         kdc_query = '_kerberos._udp.' + realm + '.'
-        return iter(query(kdc_query,'SRV')).next()
+        result = iter(query(kdc_query,'SRV')).next()
+        return 'Kdc for REALM "%s" is %s.' % (str(realm), str(result))
     except NXDOMAIN:
         raise Error('Kdc for REALM "%s" not found' % realm)
 
 def _check_krb5_passwd(realm):
     try:
         kdc_query = '_kpasswd._udp.' + realm + '.'
-        return iter(query(kdc_query,'SRV')).next()
+        result = iter(query(kdc_query,'SRV')).next()
+        return 'Kpasswd for REALM "%s" is %s.' % (str(realm), str(result))
     except NXDOMAIN:
         raise Error('Kpasswd for REALM "%s" not found' % realm)
 
 def _check_dns_CNAME(realm):
     try:
         kdc_query = 'kerberos.' + realm + '.'
-        return iter(query(kdc_query,'CNAME')).next()
+        result = iter(query(kdc_query,'CNAME')).next()
+        return 'CNAME record for kerberos is %s.' % str(result)
     except NXDOMAIN:
-        raise Error(' CNAME record for kerberos not found')
+        raise Error('CNAME record for kerberos not found')
 
 def _check_dns_A(realm):
     try:
         kdc_query = realm + '.'
-        return iter(query(kdc_query,'A')).next()
+        result = iter(query(kdc_query,'A')).next()
+        return result, 'A record for REALM "%s" is %s.' % (str(realm), str(result))
     except NXDOMAIN:
         raise Error(' A record for REALM was not found')
 
+
 def _check_dns_PTR(realm):
-    ip = _check_dns_A(realm)
+    ip, _ = _check_dns_A(realm)
     try:
         rev_ip = str(ip).split('.')
         rev_ip.reverse()
         rev_ip = string.join(rev_ip, '.')
         rev_ip = rev_ip + '.in-addr.arpa.'
         kdc_query = rev_ip
-        return iter(query(kdc_query,'PTR')).next()
+        result = iter(query(kdc_query,'PTR')).next()
+        return 'PTR record for ip "%s" is %s.' % ( str(ip), str(result))
     except NXDOMAIN:
-        raise Error(' PTR record for REALM was not found')
+        raise Error(' PTR record for ip %s was not found' % str(ip))
 
 def check_krb5_lookup():
-    realm = _check_krb5_realm()
-    _check_krb5_kdc(realm)
-    _check_krb5_passwd(realm)
-    _check_dns_CNAME(realm)
-    _check_dns_A(realm)
-    _check_dns_PTR(realm)
+    success = []
+    error = []
+    try:
+        realm, realmstr = _check_krb5_realm()
+    except Error, e:
+        error.append('%s' % e)
+    else:
+        success.append('%s' % realmstr)
+
+    try:
+        kdc = _check_krb5_kdc(realm)
+    except Error, e:
+        error.append('%s' % e)
+    else:
+        success.append('%s' % kdc)
+
+    try:
+        kpasswd = _check_krb5_passwd(realm)
+    except Error, e:
+        error.append('%s' % e)
+    else:
+        success.append('%s' % kpasswd)
+
+    try:
+        cname = _check_dns_CNAME(realm)
+    except Error, e:
+        error.append('%s' % e)
+    else:
+        success.append('%s' % cname)
+
+    try:
+        _, arecord = _check_dns_A(realm)
+    except Error, e:
+        error.append('%s' % e)
+    else:
+        success.append('%s' % arecord)
+
+    try:
+        ptrrec = _check_dns_PTR(realm)
+    except Error, e:
+        error.append('%s' % e)
+    else:
+        success.append('%s' % ptrrec)
+    return success, error
 
 def _check_host(host = None):
     if not host:
         try:
             host = hostname.getname()
+            return "Hostname: %s" % host
         except:
-            raise Error(' Can\'t get hostname.\n Please check your network instalation.\n Aborted.')
+            raise Error('Hostname: Can\'t get hostname.\n Please check your network instalation.\n Aborted.')
+
 
 def _check_domain(domain = None):
     if not domain:
         try:
             domain = hostname.getdomain()
         except:
-            raise Error(' Can\'t get domain name.\n Please check your network instalation.\n Aborted.')
+            raise Error('Domain: Can\'t get domain name.\n Please check your network instalation.\n Aborted.')
 
     if domain.find('.') < 0:
-        raise Error(' Can\'t enter in to \'%s\' domain.\n Please check your domain name: it must have points.\n Aborted.' % domain)
+        raise Error('Domain: Can\'t enter in to \'%s\' domain.\n Please check your domain name: it must have points.\n Aborted.' % domain)
+
     if len(domain) < 2:
-        raise Error(' Can\'t enter in to \'%s\' domain.\n Please check your domain name: it is too small.\n Aborted.' % domain)
+        raise Error('Domain: Can\'t enter in to \'%s\' domain.\n Please check your domain name: it is too small.\n Aborted.' % domain)
+
     if domain.endswith('.localdomain') or domain.endswith('.localdomain.'):
-        raise Error(' Can\'t enter in to \'%s\' domain.\n Please check your domain name: it must not be \'localdomain\'.\n Aborted.' % domain)
+        raise Error('Domain: Can\'t enter in to \'%s\' domain.\n Please check your domain name: it must not be \'localdomain\'.\n Aborted.' % domain)
+
+    return "Domain: %s" % domain
 
 def _check_fqdn(fqdn = None):
     if not fqdn:
         try:
             fqdn = hostname.getfqdn()
         except:
-            raise Error(' Can\'t get full hostname.\n Please check your network instalation.\n Aborted.')
+            raise Error('Full hostname: Can\'t get full hostname.\n Please check your network instalation.\n Aborted.')
+
     if fqdn.find('.') < 0:
-        raise Error(' Uncorrect full hostname \'%s\'.\n Please check your full hostname: it must have points.\n Aborted.' % fqdn)
+        raise Error('Full hostname: Uncorrect full hostname \'%s\'.\n Please check your full hostname: it must have points.\n Aborted.' % fqdn)
+
     if len(fqdn) < 3:
-        raise Error(' Uncorrect full hostname \'%s\'.\n Please check your full hostname: it is too small.\n Aborted.' % fqdn)
+        raise Error('Full hostname: Uncorrect full hostname \'%s\'.\n Please check your full hostname: it is too small.\n Aborted.' % fqdn)
+
+    return "Full hostname: %s" % fqdn
 
 def check_host_domain():
-    _check_host()
-    _check_domain()
-    _check_fqdn()
+    success = []
+    error = []
+    try:
+        hostname = _check_host()
+    except Error, e:
+        error.append('%s' % e)
+    else:
+        success.append('%s' % hostname)
 
+    try:
+        domainname = _check_domain()
+    except Error, e:
+        error.append('%s' % e)
+    else:
+        success.append('%s' % domainname)
+
+    try:
+        fullhostname = _check_fqdn()
+    except Error, e:
+        error.append('%s' % e)
+    else:
+        success.append('%s' % fullhostname)
+
+    return success, error
